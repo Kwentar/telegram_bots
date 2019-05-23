3a7d68ef853c37be165156e4b46249e4e4f201f5
Aleksey <alekseev.yeskela@gmail.com>
2019-05-23 15:51:26 +0300

    Create config.py
    template config file

---
+++ config.py
@@ -0,0 +1,10 @@
+import logging
+
+
+tg_github_user_token = ''  # telegram bot token, for github bot
+tg_reminder_token = ''  # telegram bot token, for reminder bot
+tg_room_vs_plan_token = ''  # telegram bot token, for tests
+address = 'socks5://<user>:<password>@<IP>:<port>'  # socks5 proxy for test tg bots in Russia
+github_token = ''  # github token for github bot (need for API)
+github_user = ''  #github user name for github bot (need for API)
+logging_level = logging.DEBUG
